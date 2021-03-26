64ab84403956a1793da07594f51fbef2fcd1bf25
Janusz-Alexey <janush.alexey@gmail.com>
2021-03-27 00:19:53 +0300

    Update

--- main.py
+++ main.py
@@ -9,13 +9,27 @@ x = 0
 D = 0
 x1 = 0
 x2 = 0
+
+class ClassName(object):
+	"""docstring for ClassName"""
+	def __init__(self, arg):
+		super(ClassName, self).__init__()
+		self.arg = arg
+
+def function(type):
+	if A = 0:
+		type = 1
+	elif B = 0:
+		type = 2
+	elif C = 0:
+		type = 3
+	elif B = C = 0:
+		type =4
+
 def equation():
     global D
     D = B * B - 4 * A * C
     print("Дискриминант = ",D)
-
-
-equation()
 
 def x2():
     if D < 0:
@@ -31,4 +45,7 @@ def x2():
         x2 = ((-B) - math.sqrt(D)) / (2 * A)
         print("x1:=", x1)
         print("x2:=", x2)
+
+equation()
+
 x2()
