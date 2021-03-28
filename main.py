41392dc5c8fcd1566651e01640c074ab79629fc1
Janusz-Alexey <janush.alexey@gmail.com>
2021-03-28 21:39:41 +0300

    Upddate_№3

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ D = 0
 x1 = 0
 x2 = 0
 Boss = 0
+helper = 0
 A = int(input('Введите A:='))
 B = int(input("Введите B:="))
 C = int(input("Введите C:="))
@@ -24,25 +25,55 @@ def funcfion():
         Boss = 3
     elif B == C == 0:
         Boss = 4
+    elif B == A == 0:
+        Boss = 5
+    elif C == A == 0:
+        Boss = 6
+    elif A == B == C == 0:
+        Boss = 7
+    else:
+        Boss = 8
+
+
 
 
 def Happy():
-    if Boss == 4:
+    if Boss == 8:
+        global helper
+        helper = 1
+    elif Boss == 7:
         global x
+        x = str('Любое число')
+        print(x)
+    elif Boss == 6:
         x = 0
+        print(x)
+    elif Boss == 5:
+        x = C
+        print(x)
+    elif Boss == 4:
+        x = 0
+        print(x)
     elif Boss == 3:
         global x1
         global x2
         x1 = 0
         x2 = -B / A
-    elif Boss == 2 ((-C / A) > 0):
-        x1 = math.sqrt(-C / A)
-        x2 = -(math.sqrt(-C / A))
-    elif Boss == 2 ((-C / A) < 0):
-        print("Нет решения")
+        print(x1)
+        print(x2)
     elif Boss == 1:
         x = -B / C
-    pass
+        print(x)
+    else:
+        pass
+def happy_2():
+    if Boss == 2:
+        global x1
+        global x2
+        x1 = math.sqrt(-C / A)
+        x2 = -(math.sqrt(-C / A))
+    elif 0 > (-C / A):
+        x1 = print("Нет решения")
 
 
 def equation():
@@ -67,71 +98,6 @@ def x2():
         print("x2:=", x2)
 
 
-import math
-
-A: int = 0
-B: int = 0
-C: int = 0
-
-
-x = 0
-D = 0
-x1 = 0
-x2 = 0
-Boss = 0
-A = int(input('Введите A:='))
-B = int(input("Введите B:="))
-C = int(input("Введите C:="))
-
-def funcfion():
-    if int (A) == 0:
-        global Boss
-        Boss == 1
-    elif int (B) ==0:
-        Boss == 2
-    elif int (C) == 0:
-        Boss = 3
-    elif B == C == 0:
-        Boss = 4
-
-
-def Happy():
-    if Boss == 4:
-        global x
-        x = 0
-    elif Boss == 3:
-        global x1
-        global x2
-        x1 = 0
-        x2 = -B / A
-    elif Boss == 2 ((-C / A) > 0):
-        x1 = math.sqrt(-C / A)
-        x2 = -(math.sqrt(-C / A))
-    elif Boss == 2 ((-C / A) < 0):
-        print("Нет решения")
-    elif Boss == 1:
-        x = -B / C
-    pass
-
-
-def equation():
-    global D
-    D = B * B - 4 * A * C
-    print("Дискриминант = ", D)
-
-
-def x2():
-    if D < 0:
-        print("нет корней")
-    elif D == 0:
-        global x
-        x = B / (A * 2)
-        print(x)
-    elif D > 1:
-        global x1
-        global x2
-        x1 = ((-B) + math.sqrt(D)) / (2 * A)
-        x2 = ((-B) - math.sqrt(D)) / (2 * A)
-        print("x1:=", x1)
-        print("x2:=", x2)
-print("alex")
+Happy()
+happy_2()
+x2()
